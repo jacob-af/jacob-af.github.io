794dc7a7a7a95995d3202efe87e19f89da36cef7
jacob-af <jfeitler@gmail.com>
2021-04-28 13:09:03 -0400

    refactor portfolio to use single and changin data

---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./portfolio.css";
+import Project from "../../components/Project";
+import portfolioData from "../../portfolio-data";
+
+const Portfolio = () => {
+  const [currentProject, setCurrentProject] = useState(0);
+
+  return (
+    <div
+      className="animate__animated animate__fadeInUpBig row justify-content-center"
+      id="portfolio"
+    >
+      <div className="col portfolio">
+        <h1>Recent Projects</h1>
+        <div>
+          <div>
+            <Project project={portfolioData[currentProject]} />
+            <Link
+              className="carousel-control-prev"
+              id="arrow"
+              data-slide="prev"
+              to="/portfolio"
+              onClick={() =>
+                currentProject > 0
+                  ? setCurrentProject(currentProject - 1)
+                  : setCurrentProject(portfolioData.length - 1)
+              }
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Previous</span>
+            </Link>
+            <Link
+              className="carousel-control-next arrow"
+              data-slide="next"
+              to="/portfolio"
+              onClick={() =>
+                currentProject < portfolioData.length - 1
+                  ? setCurrentProject(currentProject + 1)
+                  : setCurrentProject(0)
+              }
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Next</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
