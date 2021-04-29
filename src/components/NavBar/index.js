be48ae9b8bc627823d311650a6766ead0d9eae7e
jacob-af <jfeitler@gmail.com>
2021-04-29 12:32:42 -0400

    separated out navbar brand styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./navbar.css";
 
 const NavBar = () => {
   const [show, setShow] = useState("");
